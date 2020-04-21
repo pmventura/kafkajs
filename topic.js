a04c657e6f95941891b1323e9ff99948e7db2cee
Paul Ventura <christianpaulventura@gmail.com>
2020-04-22 05:35:09 +0800

    create topics with 2 partitions

--- topic.js
+++ topic.js
@@ -3,6 +3,24 @@ const {Kafka} = require("kafkajs");
 
 async function run() {
     try {
+        // Create an admin connection
+        const kafka = new Kafka({
+            "clientId": "myapp",
+            "brokers": ["pmventura:9092"]
+        });
+
+        const admin = kafka.admin();
+        console.log("Connnectingg......");
+        await admin.connect();
+        console.log("Connected!");
+
+        // A-M, N-Z
+        await admin.createTopics({
+            "topics": [{
+                "topic": "Users",
+                "numPartitions": 2
+            }]
+        });
 
     }
     catch(ex) {
